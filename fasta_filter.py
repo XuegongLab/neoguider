91da1e6de808e7369ade6778977e54537cf9e189
ZhaoXiaofei <cndfeifei@hotmail.com>
2023-08-22 21:49:48 +0800

    Let fasta_filter.py have the functionality to annotate each FASTA record with HLA alleles (this functionality is not used so far).

--- fasta_filter.py
+++ fasta_filter.py
@@ -27,7 +27,7 @@ def faa2newfaa(arg):
         ret.append((new_hdr, new_seq))
     return ret
 
-def output(fhdr, fseq, alphabet, tpm_thres):
+def output(fhdr, fseq, alphabet, tpm_thres, hla):
     tpm = 0
     for i, tok in enumerate(fhdr.split()):
         if i > 0 and len(tok.split('=')) == 2:
@@ -40,7 +40,8 @@ def output(fhdr, fseq, alphabet, tpm_thres):
         if ch not in alphabet:
             logging.warning(F'The FASTA record (header={fhdr}, seq={fseq}) does not use the alphabet ({alphabet}), so skipping this record. ')
             return ch
-    print(fhdr)
+    if hla: print(fhdr + ' HLA=' + hla)
+    else: print(fhdr)
     print(fseq)
     return 0
 
@@ -50,18 +51,19 @@ def main():
     parser = argparse.ArgumentParser(description = 'Read FASTA records from stdin, keep records with sequences in --alphabet, and write FASTA records to stdout. ',
             formatter_class = argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-a', '--alphabet', default = ALPHABET, help = 'The alphabet that each kept FASTA sequence must conform to. ')
+    parser.add_argument('-h', '--hla', default = '', type = float, help = 'String of comma-separated HLA alleles to be added as comment to each FASTA sequence. ')
     parser.add_argument('-t', '--tpm', default = 1.0, type = float, help = 'Transcript per million (TPM) below which the FASTA sequence is filtered out. ')
-
+    
     args = parser.parse_args()
     fhdr = None
     for line in sys.stdin:
         if line.startswith('>'):
-            if fhdr: output(fhdr, fseq, args.alphabet, args.tpm)
+            if fhdr: output(fhdr, fseq, args.alphabet, args.tpm, args.hla)
             fhdr = line.strip()
             fseq = []
         else:
             fseq.append(line.strip())
-    if fhdr: output(fhdr, fseq, args.alphabet, args.tpm)
+    if fhdr: output(fhdr, fseq, args.alphabet, args.tpm, args.hla)
 
 if __name__ == '__main__':
     main()
