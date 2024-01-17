e1a85f89763dd27913b805ea29ca5d1c2cdc0a24
ZhaoXiaofei <cndfeifei@hotmail.com>
2024-01-17 15:51:31 +0800

    In neopredictor.py:     use copy.deepcopy on sklearn classifiers,     renamed nhh_train to neopredictor in log msg,     filter by peplen only happens in filtdf instead of at both filtdf and filter_testdf (redundant),     let filterdf and filter_testdf both be aware of min-train-data by assuming that PepTrace column is present if and only if min-train-data is not given as input,     renamed the column F_LOG_N_VALIDATION into ln_NumTested,     removed a lot of unused columns,     standardized suffix (now by default the suffix is prediction),     renamed OtherPredictedProb into PredictedProbWithOtherMethod_<n>,     generated .top1000 files to avoid going through huge files,     use DataFrame.assign instead of DataFrame[COLUMN_NAME]=VALUE,     added peplens,ncores,baseline,feature-sets,mintrain as additional cmd-line args to this program,     use training matrix rounded to five decimal places,     put ln_NumTested in excluded_cols for better extrapolation,     quick-fix for a rare runtime error if both --model and --train are not specified.

--- neopredictor.py
+++ neopredictor.py
@@ -1,4 +1,4 @@
-import argparse, json, logging, multiprocessing, os, pickle, pprint, sys
+import argparse, copy, json, logging, multiprocessing, os, pickle, pprint, sys
 
 import numpy as np
 import pandas as pd
@@ -19,7 +19,7 @@ BIG_INT = 2**30
 SCALERS = [QuantileTransformer(), StandardScaler()]
 PREDICTORS = [LogisticRegression(), GradientBoostingClassifier()]
 
-logging.basicConfig(format=('nhh_train %(asctime)s - %(message)s'), level=logging.DEBUG)
+logging.basicConfig(format=('neopredictor %(asctime)s - %(message)s'), level=logging.DEBUG)
 
 def u2d(s): return '--' + s.replace('_', '-')
 def isna(arg): return arg in [None, '', 'NA', 'Na', 'N/A', 'None', 'none', '.']
@@ -36,20 +36,6 @@ def aaseq2canonical(aaseq): return aaseq.upper().replace('U', 'X').replace('O', 
 #   http://book.bionumbers.org/how-many-chromosome-replications-occur-per-generation/
 #   https://en.wikipedia.org/wiki/Cell_division#cite_note-8
 # Note: Probability of immunogenicity is not spearmanR-correlated with Foreignness, so Foreignness is not used. 
-# features = ['%Rank_EL', 'MT_BindAff', 'Quantification', 'BindStab', 'Agretopicity', 'Foreignness']
-featuresA0 = (['%Rank_EL', 'MT_BindAff', 'Quantification', 'BindStab', 'Agretopicity'])
-featuresA1 = (['%Rank_EL', 'MT_BindAff', 'Quantification', 'BindStab', 'Agretopicity', 
-# 'F_RankEL_LT0.5_ValF', 'F_RankEL_LT2.0_ValF',
-'F_LOG_N_VALIDATION',
-#'F_0_agr_TPM_SPEARMAN_sgn_inv_P',
-#'F_0_agr_TPM_SPEARMAN_statistic',
-#'F_1_RankEL_TPM_SPEARMAN_sgn_inv_P',
-#'F_1_RankEL_TPM_SPEARMAN_statistic',
-]
-# + [F'F_{idx}_RankEL_agr_SPEARMAN_sgn_inv_P' for idx in range(2)]
-# + [F'F_{idx}_RankEL_agr_SPEARMAN_statistic' for idx in range(2)]
-)
-listof_features = [featuresA0, featuresA1]
 
 def safediv(a, b, defaultval=np.nan): return ((a/b) if (b != 0) else defaultval)
 
@@ -66,12 +52,17 @@ def assess_top20_top50_top100_ttif_fr_auprc(df):
     PerformanceResult = namedtuple("PerformanceResult", "top20 top50 top100 TTIF FR AUPRC TFA_mean")
     return PerformanceResult(top20, top50, top100, ttif, fr, auprc, np.mean([ttif, fr, auprc]))
 
-def filtdf(df):
+def filtdf(df, peplens):
+    if not ('PepTrace' in df.columns): return df
     df = df.loc[~pd.isna(df['PepTrace']),:].copy()
     df['Agretopicity'] = df['Agretopicity'].fillna(1e-9)
+    MT_pep_col = ('MT_pep' if 'MT_pep' in df.columns else 'MT_pep_x')
+    is_kept = df[MT_pep_col].str.len().isin(peplens)
+    df = df.loc[is_kept,:]
     return df
 
 def filter_testdf(df):
+    if not ('PepTrace' in df.columns): return df
     is_SNV_detected_from_RNA = df['Identity'].str.startswith('SNV_R')
     is_FSV_detected_from_RNA = df['Identity'].str.startswith('FSV_R')
     is_INS_detected_from_RNA = df['Identity'].str.startswith('INS_R')
@@ -79,25 +70,24 @@ def filter_testdf(df):
     is_FUS = (df['Identity'].str.startswith('FU_') | df['Identity'].str.startswith('FUS_'))
     is_SP  = (df['Identity'].str.startswith('SP_') | df['Identity'].str.startswith('SPL_'))
     is_kept = ~(is_SNV_detected_from_RNA | is_FSV_detected_from_RNA | is_INS_detected_from_RNA | is_DEL_detected_from_RNA | is_FUS | is_SP)
-    ret = df.loc[is_kept, :]
-    return ret
+    df = df.loc[is_kept, :]
+    return df
 
 def compute_are_in_cum(df):
     if 'VALIDATED' in df.columns: 
         are_in_cum = np.where(df['VALIDATED'] >= 0, 1, 0)
     else:
-        # are_in_cum = np.where(df['%Rank_EL'] < 2.0, 1, 0)
+        # are_in_cum = np.where(df['%Rank_EL'] < 2.0, 1, 0) # this is supposed to be better but is not common practice yet
         are_in_cum = np.where(df['MT_BindAff'] < 500.0, 1, 0)
-    # df['are_in_cum'] = are_in_cum
-    df['F_RankEL_LT0.5_ValF'] = ((sum(np.where(df['%Rank_EL'] < 0.5, 1, 0) * are_in_cum) / sum(are_in_cum)) if sum(are_in_cum) else -1)
-    df['F_RankEL_LT2.0_ValF'] = ((sum(np.where(df['%Rank_EL'] < 2.0, 1, 0) * are_in_cum) / sum(are_in_cum)) if sum(are_in_cum) else -1)
-    df['F_LOG_N_VALIDATION'] = (np.log(sum(are_in_cum)) if sum(are_in_cum) else 0)
-    for idx, selector in enumerate([[1] * len(df), are_in_cum]):
-        res = stats.spearmanr(
-            [x for (x,y) in zip(df['%Rank_EL'],     selector) if y],
-            [x for (x,y) in zip(df['Agretopicity'], selector) if y])
-        df[F'F_{idx}_RankEL_agr_SPEARMAN_sgn_inv_P'] = nan_replace(np.sign(res.statistic) / max((1e-100, res.pvalue)), 0)
-        df[F'F_{idx}_RankEL_agr_SPEARMAN_statistic'] = nan_replace(res.statistic, 0)
+    df['InTested_RankEL_LT0.5_frac'] = ((sum(np.where(df['%Rank_EL'] < 0.5, 1, 0) * are_in_cum) / sum(are_in_cum)) if sum(are_in_cum) else -1)
+    df['InTested_RankEL_LT2.0_frac'] = ((sum(np.where(df['%Rank_EL'] < 2.0, 1, 0) * are_in_cum) / sum(are_in_cum)) if sum(are_in_cum) else -1)
+    df['ln_NumTested'] = (np.log(sum(are_in_cum)) if sum(are_in_cum) else 0)
+    #for idx, selector in enumerate([[1] * len(df), are_in_cum]):
+    #    res = stats.spearmanr(
+    #        [x for (x,y) in zip(df['%Rank_EL'],     selector) if y],
+    #        [x for (x,y) in zip(df['Agretopicity'], selector) if y])
+    #    df[F'F_{idx}_RankEL_agr_SPEARMAN_sgn_inv_P'] = nan_replace(np.sign(res.statistic) / max((1e-100, res.pvalue)), 0)
+    #    df[F'F_{idx}_RankEL_agr_SPEARMAN_statistic'] = nan_replace(res.statistic, 0)
     return df, are_in_cum
 
 def applyF(arr, f=sum):
@@ -105,39 +95,40 @@ def applyF(arr, f=sum):
     else: return np.nan
 
 def patientwise_predict(tuple_arg):
-    ilrs, pipelines, infile, suffix = tuple_arg
+    ilrs, pipelines, infile, suffix, peplens, baseline, listof_features = tuple_arg
     
     outpref = infile + '.' + suffix
-    logging.info(F'InFile={infile} OutPref={outpref}')
+    logging.info(F'InFile={infile} OutFile={outpref}')
     df1 = pd.read_csv(infile, sep='\t')
-    df1 = filtdf(df1)
+    df1 = filtdf(df1, peplens)
     df1 = filter_testdf(df1)
-    if len(df1) == 0: 
+    if len(df1) == 0:
         logging.warning(F'Skipping file={infile} because it is empty after filtering. ')
         return -1
     
-    #pipedir = (infile + '.other-pred.dir')
-    #os.system(F'mkdir -p {pipedir}')
+    df1, are_in_cum = compute_are_in_cum(df1)
+
     for i, pipeline in enumerate(pipelines):
+        if i != 0 and not ('method' in baseline.split(',')): continue
         pipedf = df1.copy()
-        y = pipeline.predict_proba(pipedf[listof_features[0]])
-        pipedf['OtherPredictedProb'] = [v[1] for v in y]
-        pipedf['Rank'] = pipedf['OtherPredictedProb'].rank(method='first', ascending=False)
+        y = pipeline.predict_proba(pipedf.loc[:, listof_features[0]])
+        pipedf['PredictedProbWithOtherMethod'] = [v[1] for v in y]
+        pipedf['Rank'] = pipedf['PredictedProbWithOtherMethod'].rank(method='first', ascending=False)
         pipedf['Rank'] = pipedf['Rank'].fillna(BIG_INT)
         pipedf = pipedf.sort_values('Rank')
         pipedf = pipedf.astype({"Rank":int})
-        # other_pred = F'{pipedir}/{i}.other-pred'
-        other_pred = F'{outpref}.other_pred.{i}'
-        pipedf.to_csv(other_pred, sep='\t', header=1, index=0, na_rep = 'NA')
+        other_pred = F'{outpref}.other_method_{i+1}.baseline'
+        pipedf.to_csv(other_pred, sep='\t', header=1, index=0, na_rep='NA')
+        pipedf.iloc[range(min((len(pipedf),1000))),:].to_csv(other_pred + '.top1000', sep='\t', header=1, index=0, na_rep='NA')
+
         if 'VALIDATED' in pipedf.columns:
             evalres = assess_top20_top50_top100_ttif_fr_auprc(pipedf)            
             with open(other_pred + '.performance', 'w') as file:
                 json.dump(evalres._asdict(), file, indent=2)
-            #for f.write(json.dumps evalres._asdict)
-    df, are_in_cum = compute_are_in_cum(df1)
-    #test_arr = list(filter(lambda x:(x>0), np.where(df['%Rank_EL'] < 0.5, 1, 0) * are_in_cum * (df['Quantification'] + 1e-50)))
-    #print(F'test_array={test_arr}')
-        
+    df = df1
+     
+    # These features have been tested for calibrating probabilities, and none of them performs better than ln_NumTested.
+    '''
     for idx, selector in enumerate([np.array([1] * len(df)), are_in_cum]):
         res = stats.spearmanr(
             [x for (x,y) in zip(df['%Rank_EL'],       selector) if y], 
@@ -186,11 +177,12 @@ def patientwise_predict(tuple_arg):
     df['0_RankEL_10th'] = rank010_EL
     #df['RankEL_20st'] = rank020_EL
     df['0_RankEL_100th'] = rank100_EL
-    
+    '''
     for i, (features, ilr) in enumerate(zip(listof_features, ilrs)):
+        if i != 0 and not ('feature' in baseline.split(',')): continue
         y = ilr.predict_proba(df[features])
-        if i < len(listof_features) - 1:
-            df[F'PredictedProb_{i+1}'] = [v[1] for v in y]
+        if i != 0:
+            df[F'PredictedProbWithOtherFeatureSet_{i}'] = [v[1] for v in y]
             continue
         df['PredictedProbability'] = [v[1] for v in y]
         df['Rank'] = df['PredictedProbability'].rank(method='first', ascending=False)
@@ -198,48 +190,63 @@ def patientwise_predict(tuple_arg):
         df = df.sort_values('Rank')
         df = df.astype({"Rank":int})
         _, are_in_cum = compute_are_in_cum(df)
-        #data = data.drop(drop_cols, axis=1)        
         if 'VALIDATED' in df.columns: 
-            df['VALID_N_TESTED'] = sum(are_in_cum)
-            df['VALID_CUMSUM'] = np.cumsum(df['VALIDATED'] * are_in_cum)
+            VALID_N_TESTED = sum(are_in_cum)
+            VALID_CUMSUM = np.cumsum(df['VALIDATED'] * are_in_cum)
         else:
-            df['VALID_N_TESTED'] = -1
-            df['VALID_CUMSUM'] = -1
-        df['PROBA_CUMSUM'] = np.cumsum(df['PredictedProbability'] * are_in_cum)
+            VALID_N_TESTED = -1
+            VALID_CUMSUM = -1
+        PROBA_CUMSUM = np.cumsum(df['PredictedProbability'] * are_in_cum)
         # df['BindAff_LessThan100_NUM'] = sum(np.where(df['MT_BindAff'] < 100, 1, 0) * are_in_cum) / sum(are_in_cum)
-        #df['RankEL_LT0.5_VALFRAC'] = sum(np.where(df['%Rank_EL'] < 0.5, 1, 0) * are_in_cum) / sum(are_in_cum)
-        #df['RankEL_LT2.0_VALFRAC'] = sum(np.where(df['%Rank_EL'] < 2.0, 1, 0) * are_in_cum) / sum(are_in_cum)        
+        # df['RankEL_LT0.5_VALFRAC'] = sum(np.where(df['%Rank_EL'] < 0.5, 1, 0) * are_in_cum) / sum(are_in_cum)
+        # df['RankEL_LT2.0_VALFRAC'] = sum(np.where(df['%Rank_EL'] < 2.0, 1, 0) * are_in_cum) / sum(are_in_cum)
+        df = df.assign(VALID_N_TESTED=VALID_N_TESTED, VALID_CUMSUM=VALID_CUMSUM, PROBA_CUMSUM=PROBA_CUMSUM)
     col2last(df, 'SourceAlterationDetail')
     col2last(df, 'PepTrace')
     dropcols(df, ['BindLevel', 'BindAff'])
-    logging.info(F'N_rows={len(df)} N_cols={len(df.columns)} for tsv={outpref}.prediction')
-    df.to_csv(outpref + '.prediction', sep='\t', header=1, index=0, na_rep = 'NA')
+    logging.info(F'N_rows={len(df)} N_cols={len(df.columns)} for tsv={outpref}')
+    df.to_csv(outpref, sep='\t', header=1, index=0, na_rep='NA')
+    df.iloc[range(min((len(df),1000))),:].to_csv(outpref + '.top1000', sep='\t', header=1, index=0, na_rep='NA')
+
     if 'VALIDATED' in df.columns:
         evalres = assess_top20_top50_top100_ttif_fr_auprc(df)
-        with open(outpref + '.prediction.performance', 'w') as file:
+        with open(outpref + '.performance', 'w') as file:
             json.dump(evalres._asdict(), file, indent=2)
     return 0
 
 from datetime import datetime
 def main():
     
-    parser = argparse.ArgumentParser(description = 'ML classifier for neoepitopes', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--train',  help = 'Train .validation files. ', required = False, nargs = '*', default=[])
-    parser.add_argument('--test',   help = 'Test  .validation files. ', required = False, nargs = '*', default=[])
-    parser.add_argument('--model',  help = 'Trained model file. ', required = False)
-    parser.add_argument('--suffix', help = 'Suffix of the result files containing prediction. ', required = False)
+    parser = argparse.ArgumentParser(description='ML classifier for neoepitopes', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--train',  help='Train .validation files. ', required=False, nargs='*', default=[])
+    parser.add_argument('--test',   help='Test  .validation files. ', required=False, nargs='*', default=[])
+    parser.add_argument('--model',  help='Trained model file. ', required=False)
+    parser.add_argument('--suffix', help='Suffix of the result files containing prediction. The format of the prediction file is <--test filename>.<--suffix>). ', 
+        required = False, default = 'prediction')
+    parser.add_argument('--peplens',help='Peptide length for keeping peptides. ', required=False, default='8,9,10,11,12')
+    parser.add_argument('--ncores', help='Number of CPU cores to use for --test. ', required=False, default=12)
+    parser.add_argument('--baseline', help='Comma-separated keywords. Keyword feature: test other feature sets. Keyword method: test other methods. ', required = False, default = 'feature')
+    parser.add_argument('--feature-sets', help= 
+        'List of strings with each string (i.e., feature set) consisting of comma-separated features. '
+        'The first feature set is used by default, and all other feature sets are used as baselines. ', 
+        required=False, nargs='+', default=[
+        '%Rank_EL,MT_BindAff,Quantification,BindStab,Agretopicity,ln_NumTested',
+        '%Rank_EL,MT_BindAff,Quantification,BindStab,Agretopicity'])
+    parser.add_argument('--mintrain', help='Minimized train file to be outputted (empty string means not outputted). ', required=False, default='')
 
     args = parser.parse_args()
+    listof_features = [feature_set.split(',') for feature_set in args.feature_sets]
+    peplens = [int(x) for x in args.peplens.split(',')]
     
     script_path = os.path.realpath(__file__)
     now = datetime.now()
     dt_string = now.strftime('%Y-%m-%d-%H-%M-%S')
-    tmp_script_path = F'/tmp/{dt_string}.nhh_train.py'
-    tmp_output_path = F'/tmp/{dt_string}.nhh_train.out'
-    #tmp_script = open(tmp_script_path, 'w')
+    tmp_script_path = F'/tmp/{dt_string}.neopredictor.py'
+    tmp_output_path = F'/tmp/{dt_string}.neopredictor.out'
     os.system(F'cp {script_path} {tmp_script_path}')
+    logging.info(F'Will print predictor in json format to {tmp_output_path}')
     tmp_output = open(tmp_output_path, 'w')
-    pp = pprint.PrettyPrinter(indent=2, stream=tmp_output)
+    pp = pprint.PrettyPrinter(indent=2, stream=(tmp_output if tmp_output else sys.stdout))
     
     if args.train:
         dfs = []
@@ -247,39 +254,35 @@ def main():
             df = pd.read_csv(infile, sep='\t')
             df, are_in_cum = compute_are_in_cum(df)
             dfs.append(df)
-            logging.info(F'Finished reading {infile}. ')
+            logging.info(F'Finished reading {infile}')
         big_train_df = pd.concat(dfs)
         big_train_df['VALIDATED'] = big_train_df['VALIDATED'].astype(int)
-        big_train_df = filtdf(big_train_df)
+        big_train_df = filtdf(big_train_df, peplens)
         are_validated = ((big_train_df['VALIDATED'] == 0) | (big_train_df['VALIDATED'] == 1))
         big_train_df = big_train_df.loc[are_validated,:]
-        
-        # big_train_X1.to_csv('/tmp/big_train_X.tsv', sep = '\t')    
         big_train_y = big_train_df['VALIDATED'].astype(int)
-        # big_train_y = np.maximum(big_train_y.astype(int), 0)
-        #print(big_train_X1)
-        #print(big_train_y)
         
         logging.info(F'Finished processing input. ')
         
         pipelines = []
-        big_train_X = big_train_df[listof_features[0]]
+        big_train_X = big_train_df.loc[:, listof_features[0]].copy()
+        if args.mintrain:
+            pd.concat([big_train_X, big_train_y], axis=1).to_csv(args.mintrain, sep='\t', header=1, index=0, na_rep='NA')
+        big_train_X = big_train_X.round(5)
         for scaler in SCALERS:
             for predictor in PREDICTORS:
-                pipe = make_pipeline(scaler, predictor)
+                pipe = make_pipeline(copy.deepcopy(scaler), copy.deepcopy(predictor))
                 pipe.fit(big_train_X, big_train_y)
                 pipelines.append(pipe)
         
         ilrs = []
         for features in listof_features:
-            big_train_X = big_train_df[features]
-            iso_scaler = IsotonicLogisticRegression() # (excluded_cols=['F_LOG_N_VALIDATION']) # ()
-            #log_regression = LogisticRegression(penalty=None)
-            #ilr = make_pipeline(iso_scaler, log_regression)
-            #ilr.fit(big_train_X, big_train_y) # #ilr.fit(big_train_X, big_train_y, is_centered = False)             
-            #ilrs.append(ilr)
+            big_train_X = big_train_df.loc[:, features].copy().round(5)
+            iso_scaler = IsotonicLogisticRegression(excluded_cols=['ln_NumTested']) # excluded_cols is used for better extrapolation
             iso_scaler.fit(big_train_X, big_train_y)
             ilrs.append(iso_scaler)
+        
+        logging.info(F'Finished training. ')
 
     if args.model and args.train:
         logging.info(F'Saving the model in pickle format to {args.model}')
@@ -289,17 +292,16 @@ def main():
         logging.info(F'Loading the model in pickle format from {args.model}')
         with open(args.model, 'rb') as file:
             ilrs, pipelines = pickle.load(file)
-    
+    else:
+        ilrs, pipelines = ([], [])
     pp.pprint([(i, ilr.get_info()) for i, ilr in enumerate(ilrs)])
     
     logging.info(F'Finished fitting IsotonicLogisticRegressions. ')
     
-    p = multiprocessing.Pool(12)
-    ret = list(map(patientwise_predict, [(ilrs, pipelines, infile, args.suffix) for infile in args.test]))
-    # print(F'RET={ret}')
-    # tmp_script.close()
-    tmp_output.close()
-    logging.info(F'Finished running {sys.argv[0]}. ')
+    p = multiprocessing.Pool(args.ncores)
+    ret = list(p.map(patientwise_predict, [(ilrs, pipelines, infile, args.suffix, peplens, args.baseline, listof_features) for infile in args.test]))
+    if tmp_output: tmp_output.close()
+    logging.info(F'Finished running {sys.argv[0]}')
     
 if __name__ == '__main__':
     main()
