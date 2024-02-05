c134625e8390178d27e77d111fff457217de7660
ZhaoXiaofei <cndfeifei@hotmail.com>
2024-02-05 17:03:05 +0800

    Modified TargetEncoder.py: Implemented fit and transform methods in addition to the fit_transform method.

--- TargetEncoder.py
+++ TargetEncoder.py
@@ -6,9 +6,13 @@ import scipy
 
 class TargetEncoder:
     
-    def __init__(self, excluded_cols = [], pseudocount=0.5, *args, **kwargs):
+    def __init__(self, excluded_cols = [], pseudocount=0.5):
         """ Initialize """
         self.pseudocount = pseudocount
+        
+        self.n_neg_training_examples = -1
+        self.n_pos_training_examples = -1
+        self.colidxto01counter = None
 
     def _abbrevshow(alist, anum=5):
         if len(alist) <= anum*2: return [alist]
@@ -24,12 +28,16 @@ class TargetEncoder:
         assert X1.shape[0] < X0.shape[0], 'The number of positive examples should be less than the number of negative examples'
     
     ''' Note: this method (which uses resampling) is supposed to outperform sklearn.preprocessing.TargetEncoder.fit_transform (which uses cross-fitting)'''
-    def fit_transform(self, X1, y1, random_state=0, *args, **kwargs):
+    def fit_transform(self, X1, y1, random_state=0):
         """
             X1: categorical features
             y1: binary response
             return: numerically encoded features using the WOE (weight of evidence) encoding (https://letsdatascience.com/target-encoding/) with resampling
         """
+        self.fit(X1, y1)
+        return self.transform(X1, random_state=random_state)
+
+    def fit(self, X1, y1):
         X = np.array(X1)
         y = np.array(y1)
         self._assert_input(X, y)
@@ -37,25 +45,37 @@ class TargetEncoder:
         X1 = self.X1 = X[y==1,:]
         baseratio = (float(len(X1)) / float(len(X0)))
         logbase = np.log(baseratio)
-        prior_w = self.pseudocount
-        ret = []
-        running_rand = np.random.default_rng(random_state)
+        prior_w = self.pseudocount       
+        self.n_neg_training_examples = X0.shape[0]
+        self.n_pos_training_examples = X1.shape[0]
+        self.colidxto01counter = []
         for colidx in range(X.shape[1]):
             elements = sorted(np.unique(X[:,colidx]))
             x0counter = collections.Counter(sorted(X0[:,colidx]))
             x1counter = collections.Counter(sorted(X1[:,colidx]))
-            x0counter2 = {}
-            x1counter2 = {}
-            for ele in elements:
-                total_x_count = (x0counter[ele] + x1counter[ele])
-                noisy_x0count = scipy.stats.binom.rvs(n=total_x_count, p=x0counter[ele]/total_x_count, random_state=running_rand)
-                noisy_x1count = total_x_count - noisy_x0count
-                x0counter2[ele] = int(noisy_x0count)
-                x1counter2[ele] = int(noisy_x1count)
-            ele2or = {ele : ((x1counter2[ele] + prior_w) / (x0counter2[ele] + prior_w / baseratio)) for ele in elements}
-            ret.append([(np.log(ele2or[ele]) - logbase) for ele in X[:,colidx]])
-        return np.array(ret).transpose()
+            self.colidxto01counter.append((x0counter, x1counter))
+        return self
 
+    def transform(self, X1, random_state=0):
+        X = np.array(X1)
+        assert X.shape[1] == len(self.colidxto01counter)
+        running_rand = np.random.default_rng(random_state)
+        npc = self.pseudocount * self.n_neg_training_examples / self.n_pos_training_examples
+        ppc = self.pseudocount
+        n_training_examples = (self.n_neg_training_examples+self.n_pos_training_examples)
+        ret = []
+        for colidx in range(X.shape[1]):
+            x0counter, x1counter = self.colidxto01counter[colidx]
+            binom_rvs = [
+                scipy.stats.binom.rvs(
+                    n=n_training_examples,
+                    p=float(x1counter[k]+ppc)/(x0counter[k]+x1counter[k]+ppc+npc),
+                    random_state=running_rand
+                ) for k in X[:,colidx]]
+            targets = [(rv/float(n_training_examples)) for rv in binom_rvs]
+            ret.append(targets)
+        return np.array(ret).transpose()
+    
 def test_1():
     pp = pprint.PrettyPrinter(indent=4)
     logging.basicConfig(format='TargetEncoder %(asctime)s - %(message)s', level=logging.DEBUG)
@@ -73,8 +93,10 @@ def test_1():
     ])
     y = np.array([1,1,0,1,1,0,0,0,0,0])
     encoder = TargetEncoder()
-    X2 = encoder.fit_transform(X, y)
+    X2 = encoder.fit_transform(X, y, random_state=17)
     logging.info(F'Encoded={X2}')
+    X3 = encoder.fit_transform(X, y, random_state=997)
+    logging.info(F'Encoded={X3}')
 
 if __name__ == '__main__':
     test_1()
