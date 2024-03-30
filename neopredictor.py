477438b5b7d3809d30587c3f185a6ea29e8f6210
ZhaoXiaofei <cndfeifei@hotmail.com>
2024-03-31 00:26:40 +0800

    In neopredictor.py: replaced the np.array constructor with the pd.DataFrame and pd.Series constructors, because the pd constructors can also prevent the (ValueError: cannot set WRITEABLE flag to True of this array) error.

--- neopredictor.py
+++ neopredictor.py
@@ -386,8 +386,8 @@ def main():
         if args.mintrain:
             pd.concat([big_train_X, big_train_y], axis=1).to_csv(args.mintrain, sep='\t', header=1, index=0, na_rep='NA')
         big_train_X = big_train_X.round(5)
-        big_train_X = np.array(big_train_X)
-        big_train_y = np.array(big_train_y)        
+        big_train_X = pd.DataFrame(big_train_X)
+        big_train_y = pd.Series(big_train_y)
         if ('method' in args.baseline.split(',')):
             for predictor_name, predictor in PREDICTORS.items():
                 for scaler_name, scaler in SCALERS.items():            
