d625a9fd0c903e00ddf6c8aee7836d3cd08d17c7
ZhaoXiaofei <cndfeifei@hotmail.com>
2024-03-30 13:59:13 +0800

    In neopredictor.py: the combinations of all feature-normalization techniques in sklearn and the commonly used classifiers in sklearn, and use joblib (instead of multiprocessing which may hang for allocation of large virtual mem) and parallelization of both --train and --test instead of only --test.

--- neopredictor.py
+++ neopredictor.py
@@ -1,24 +1,92 @@
-import argparse, copy, json, logging, multiprocessing, os, pickle, pprint, sys
+import argparse, copy, json, logging, os, pickle, pprint, random, sys
+
+from joblib import Parallel, delayed # multiprocessing can hang if the virtual memory allocated is too big
 
 import numpy as np
 import pandas as pd
 
-from collections import namedtuple
+from collections import defaultdict, namedtuple
+
 from scipy import stats
 
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 from sklearn.pipeline import make_pipeline, Pipeline
-from sklearn.preprocessing import QuantileTransformer, StandardScaler #, PowerTransformer, MinMaxScaler, FunctionTransformer
+from sklearn.compose import ColumnTransformer
+# Modified from https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+from sklearn.discriminant_analysis import (LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis)
+from sklearn.ensemble import (AdaBoostClassifier, ExtraTreesClassifier, GradientBoostingClassifier, RandomForestClassifier,)
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.preprocessing import (
+    MaxAbsScaler,
+    MinMaxScaler,
+    Normalizer,
+    PowerTransformer,
+    QuantileTransformer,
+    RobustScaler,
+    StandardScaler,
+    # minmax_scale,       # same as MinMaxScaler
+    # FunctionTransformer # using user-implemented custom function
+)
+
+from xgboost import XGBClassifier
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from IsotonicLogisticRegression import IsotonicLogisticRegression
 
-BIG_INT = 2**30
-SCALERS = [QuantileTransformer(), StandardScaler()]
-PREDICTORS = [LogisticRegression(), GradientBoostingClassifier()]
+random.seed(0)
+np.random.seed(0)
 
+BIG_INT = 2**30
+
+# https://scikit-learn.org/stable/auto_examples/preprocessing/plot_all_scaling.html
+SCALERS = {    
+    'Identity'            : ColumnTransformer([], remainder='passthrough'),
+    'MaxAbsScaler'        : MaxAbsScaler(),
+    'MinMaxScaler'        : MinMaxScaler(),
+    'Normalizer'          : Normalizer(),
+    'PowerTransformer'    : PowerTransformer(),
+    'QuantileTransformer' : QuantileTransformer(random_state=0),    
+    'RobustScaler'        : RobustScaler(),
+    'StandardScaler'      : StandardScaler(),
+    'StandardTransformer' : QuantileTransformer(random_state=0, output_distribution='normal'),
+    '_KDEIsoTransformer00' : IsotonicLogisticRegression(random_state=0, fit_data_clear=True, 
+        fit_add_measure_error=False, transform_add_measure_error=False, ft_fit_add_measure_error=False, ft_transform_add_measure_error=False),
+    '_KDEIsoTransformer01' : IsotonicLogisticRegression(random_state=0, fit_data_clear=True, 
+        fit_add_measure_error=False, transform_add_measure_error=False, ft_fit_add_measure_error=False, ft_transform_add_measure_error=True ),
+    '_KDEIsoTransformer10' : IsotonicLogisticRegression(random_state=0, fit_data_clear=True, 
+        fit_add_measure_error=False, transform_add_measure_error=True,  ft_fit_add_measure_error=False, ft_transform_add_measure_error=False),
+    '_KDEIsoTransformer11' : IsotonicLogisticRegression(random_state=0, fit_data_clear=True, 
+        fit_add_measure_error=False, transform_add_measure_error=True,  ft_fit_add_measure_error=False,  ft_transform_add_measure_error=True),
+}
+
+# https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+PREDICTORS = {
+    'KN': KNeighborsClassifier(),
+    # 'KN500': KNeighborsClassifier(n_neighbors=500), # Too many ties in probabilities (density of positive examples is too low)
+    # 'SVC': SVC(probability=True), # sklearn.svm import SVC is not designed to predict probability and is not designed to handle large sample size (O(n*n) runtime, intractable)
+    # 'GP': GaussianProcessClassifier(random_state=0), # Error: numpy.core._exceptions._ArrayMemoryError: Unable to allocate 1.25 TiB for ...
+    'DT': DecisionTreeClassifier(random_state=0),
+    'RF': RandomForestClassifier(random_state=0),
+    'MLP': MLPClassifier(random_state=0),
+    'AB': AdaBoostClassifier(random_state=0),
+    'GNB': GaussianNB(),
+    'LDA': LinearDiscriminantAnalysis(),
+    'QDA': QuadraticDiscriminantAnalysis(),
+    
+    'LR' : LogisticRegression(random_state=0),
+    # Tree-based algorithms other than the ones used above:
+    'eET': ExtraTreesClassifier(random_state=0),
+    'eGB': GradientBoostingClassifier(random_state=0),
+    'eXGB': XGBClassifier(random_state=0),
+}
+
+logger = logging.getLogger(__name__)
 logging.basicConfig(format=('neopredictor %(asctime)s - %(message)s'), level=logging.DEBUG)
 
 def u2d(s): return '--' + s.replace('_', '-')
@@ -94,39 +162,67 @@ def applyF(arr, f=sum):
     if len(arr): return f(arr)
     else: return np.nan
 
-def patientwise_predict(tuple_arg):
-    ilrs, pipelines, infile, suffix, peplens, baseline, listof_features = tuple_arg
-    
+def patientwise_predict_test(tuple_arg):
+    ilrs, pipeline_names, pipelines, infile, suffix, peplens, baseline, listof_features = tuple_arg
+    logger = logging.getLogger('patientwise_predict_test')
     outpref = infile + '.' + suffix
-    logging.info(F'InFile={infile} OutFile={outpref}')
+    logger.info(F'InFile={infile} OutFile={outpref}')
+
+def patientwise_predict(tuple_arg):
+    ilrs, pipeline_names, pipelines, infile, suffix, peplens, baseline, listof_features = tuple_arg
+    logger = logging.getLogger('Test')
+    logging.basicConfig(format=(F'neo-test %(asctime)s - %(message)s'), 
+        level=logging.DEBUG)
+
+    outpref = infile + '.' + suffix
+    logger.info(F'START: infile={infile}')
     df1 = pd.read_csv(infile, sep='\t')
     df1 = filtdf(df1, peplens)
     df1 = filter_testdf(df1)
     if len(df1) == 0:
-        logging.warning(F'Skipping file={infile} because it is empty after filtering. ')
+        logger.warning(F'Skipping file={infile} because it is empty after filtering. ')
         return -1
     
     df1, are_in_cum = compute_are_in_cum(df1)
-
-    for i, pipeline in enumerate(pipelines):
-        if i != 0 and not ('method' in baseline.split(',')): continue
+    evalres3 = {}
+    evaldict = defaultdict(list)
+    for i, (pipeline_name, pipeline) in enumerate(zip(pipeline_names, pipelines)):
+        if i != 0 and not ('method' in baseline.split(',')): continue        
         pipedf = df1.copy()
+        if len(pipedf) > 50*1000: logger.info(F'pipe={pipeline_name:30} {(i+1):3}/{len(pipelines)} {infile}')
         y = pipeline.predict_proba(pipedf.loc[:, listof_features[0]])
         pipedf['PredictedProbWithOtherMethod'] = [v[1] for v in y]
         pipedf['Rank'] = pipedf['PredictedProbWithOtherMethod'].rank(method='first', ascending=False)
         pipedf['Rank'] = pipedf['Rank'].fillna(BIG_INT)
         pipedf = pipedf.sort_values('Rank')
         pipedf = pipedf.astype({"Rank":int})
-        other_pred = F'{outpref}.other_method_{i+1}.baseline'
+        pipedf['ML_pipeline'] = pipeline_name
+        other_pred = F'{outpref}.other_method_{(i+1):03d}.baseline'
         pipedf.to_csv(other_pred, sep='\t', header=1, index=0, na_rep='NA')
         pipedf.iloc[range(min((len(pipedf),1000))),:].to_csv(other_pred + '.top1000', sep='\t', header=1, index=0, na_rep='NA')
 
         if 'VALIDATED' in pipedf.columns:
-            evalres = assess_top20_top50_top100_ttif_fr_auprc(pipedf)            
-            with open(other_pred + '.performance', 'w') as file:
-                json.dump(evalres._asdict(), file, indent=2)
+            evalres = assess_top20_top50_top100_ttif_fr_auprc(pipedf)
+            evalres2 = evalres._asdict()
+            evalres3[pipeline_name] = evalres2
+            
+            transform_name, ml_model_name = pipeline_name.split('/')
+            evaldict['infile'].append(infile)
+            evaldict['feature_transform'].append(transform_name)
+            evaldict['ML_model_name'].append(ml_model_name)
+            for k, v in copy.deepcopy(evalres._asdict()).items():
+                evaldict[k].append(v)
+        del pipedf
+    if evalres3:
+        with open(outpref + '.methods.performance', 'w') as file:
+            json.dump(evalres3, file, indent=2)
+    if evaldict:
+        with open(outpref + '.methods.performance.tsv', 'w') as file:
+            eval_df = pd.DataFrame.from_dict(evaldict)
+            eval_df.to_csv(file, sep='\t', header=1, index=0, na_rep='NA')
+    
     df = df1
-     
+    
     # These features have been tested for calibrating probabilities, and none of them performs better than ln_NumTested.
     '''
     for idx, selector in enumerate([np.array([1] * len(df)), are_in_cum]):
@@ -200,21 +296,43 @@ def patientwise_predict(tuple_arg):
         # df['BindAff_LessThan100_NUM'] = sum(np.where(df['MT_BindAff'] < 100, 1, 0) * are_in_cum) / sum(are_in_cum)
         # df['RankEL_LT0.5_VALFRAC'] = sum(np.where(df['%Rank_EL'] < 0.5, 1, 0) * are_in_cum) / sum(are_in_cum)
         # df['RankEL_LT2.0_VALFRAC'] = sum(np.where(df['%Rank_EL'] < 2.0, 1, 0) * are_in_cum) / sum(are_in_cum)
-        df = df.assign(VALID_N_TESTED=VALID_N_TESTED, VALID_CUMSUM=VALID_CUMSUM, PROBA_CUMSUM=PROBA_CUMSUM)
+        df = df.assign(VALID_N_TESTED=VALID_N_TESTED, VALID_CUMSUM=VALID_CUMSUM, PROBA_CUMSUM=PROBA_CUMSUM, ML_pipeline='default_ML_pipe')
     col2last(df, 'SourceAlterationDetail')
     col2last(df, 'PepTrace')
     dropcols(df, ['BindLevel', 'BindAff'])
-    logging.info(F'N_rows={len(df)} N_cols={len(df.columns)} for tsv={outpref}')
+    logger.info(F'N_rows={len(df)} N_cols={len(df.columns)} for {outpref}')
     df.to_csv(outpref, sep='\t', header=1, index=0, na_rep='NA')
     df.iloc[range(min((len(df),1000))),:].to_csv(outpref + '.top1000', sep='\t', header=1, index=0, na_rep='NA')
 
     if 'VALIDATED' in df.columns:
         evalres = assess_top20_top50_top100_ttif_fr_auprc(df)
+        evalres2 = evalres._asdict()
+        evalres2['ML_pipeline'] = 'default_ML_pipe'
         with open(outpref + '.performance', 'w') as file:
-            json.dump(evalres._asdict(), file, indent=2)
+            json.dump(evalres2, file, indent=2)
+        evaldict = defaultdict(list)
+        evaldict['infile'].append(infile)
+        evaldict['feature_transform'].append('default_transform')
+        evaldict['ML_model_name'].append('default_model')
+        for k, v in copy.deepcopy(evalres._asdict()).items():
+            evaldict[k].append(v)
+        with open(outpref + '.performance.tsv', 'w') as file:
+            eval_df = pd.DataFrame.from_dict(evaldict)
+            eval_df.to_csv(file, sep='\t', header=1, index=0, na_rep='NA')
+    logger.info(F'END:   infile={infile}')
     return 0
 
 from datetime import datetime
+def mapfunc(tuple_arg):    
+    pipename, pipe, big_train_X, big_train_y = tuple_arg
+    logger = logging.getLogger('Training')
+    logging.basicConfig(format=(F'neopredictor-train %(asctime)s - %(message)s'), 
+        level=logging.DEBUG)
+    logger.info(F'START: {pipename}')
+    pipe.fit(big_train_X, big_train_y)
+    logger.info(F'END:   {pipename}')
+    return pipe
+
 def main():
     
     parser = argparse.ArgumentParser(description='ML classifier for neoepitopes', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -224,7 +342,7 @@ def main():
     parser.add_argument('--suffix', help='Suffix of the result files containing prediction. The format of the prediction file is <--test filename>.<--suffix>). ', 
         required = False, default = 'prediction')
     parser.add_argument('--peplens',help='Peptide length for keeping peptides. ', required=False, default='8,9,10,11,12')
-    parser.add_argument('--ncores', help='Number of CPU cores to use for --test. ', required=False, default=12)
+    parser.add_argument('--ncores', help='Number of CPU cores to use for --train and --test. The special numbers -2 and 0 mean using one and all CPUs, respectively. ', required=False, type=int, default=16)
     parser.add_argument('--baseline', help='Comma-separated keywords. Keyword feature: test other feature sets. Keyword method: test other methods. ', required = False, default = 'feature')
     parser.add_argument('--feature-sets', help= 
         'List of strings with each string (i.e., feature set) consisting of comma-separated features. '
@@ -244,7 +362,7 @@ def main():
     tmp_script_path = F'/tmp/{dt_string}.neopredictor.py'
     tmp_output_path = F'/tmp/{dt_string}.neopredictor.out'
     os.system(F'cp {script_path} {tmp_script_path}')
-    logging.info(F'Will print predictor in json format to {tmp_output_path}')
+    logger.info(F'Will print predictor in json format to {tmp_output_path}')
     tmp_output = open(tmp_output_path, 'w')
     pp = pprint.PrettyPrinter(indent=2, stream=(tmp_output if tmp_output else sys.stdout))
     
@@ -254,7 +372,7 @@ def main():
             df = pd.read_csv(infile, sep='\t')
             df, are_in_cum = compute_are_in_cum(df)
             dfs.append(df)
-            logging.info(F'Finished reading {infile}')
+            logger.info(F'Finished reading {infile}')
         big_train_df = pd.concat(dfs)
         big_train_df['VALIDATED'] = big_train_df['VALIDATED'].astype(int)
         big_train_df = filtdf(big_train_df, peplens)
@@ -262,19 +380,24 @@ def main():
         big_train_df = big_train_df.loc[are_validated,:]
         big_train_y = big_train_df['VALIDATED'].astype(int)
         
-        logging.info(F'Finished processing input. ')
+        logger.info(F'Finished processing input. ')
         
+        pipeline_names = []
         pipelines = []
         big_train_X = big_train_df.loc[:, listof_features[0]].copy()
         if args.mintrain:
             pd.concat([big_train_X, big_train_y], axis=1).to_csv(args.mintrain, sep='\t', header=1, index=0, na_rep='NA')
         big_train_X = big_train_X.round(5)
-        for scaler in SCALERS:
-            for predictor in PREDICTORS:
+        for predictor_name, predictor in PREDICTORS.items():
+            for scaler_name, scaler in SCALERS.items():            
                 pipe = make_pipeline(copy.deepcopy(scaler), copy.deepcopy(predictor))
-                pipe.fit(big_train_X, big_train_y)
                 pipelines.append(pipe)
-        
+                pipeline_names.append((scaler_name + '/' +  predictor_name))
+        map_args = [(pipename, pipe, big_train_X, big_train_y) for (pipename, pipe) in zip(pipeline_names, pipelines)]
+        if args.ncores == -2:
+            pipelines = list(map(mapfunc, [(pipename, pipe, big_train_X, big_train_y) for (pipename, pipe) in zip(pipeline_names, pipelines)]))
+        else:
+            pipelines = Parallel(n_jobs=args.ncores)(delayed(mapfunc)(arg) for arg in map_args)
         ilrs = []
         for features in listof_features:
             big_train_X = big_train_df.loc[:, features].copy().round(5)
@@ -282,26 +405,30 @@ def main():
             iso_scaler.fit(big_train_X, big_train_y)
             ilrs.append(iso_scaler)
         
-        logging.info(F'Finished training. ')
+        logger.info(F'Finished training. ')
 
     if args.model and args.train:
-        logging.info(F'Saving the model in pickle format to {args.model}')
+        logger.info(F'Saving the model in pickle format to {args.model}')
         with open(args.model, 'wb') as file:
-            pickle.dump([ilrs, pipelines], file)
+            pickle.dump([ilrs, pipeline_names, pipelines], file)
     elif args.model:
-        logging.info(F'Loading the model in pickle format from {args.model}')
+        logger.info(F'Loading the model in pickle format from {args.model}')
         with open(args.model, 'rb') as file:
-            ilrs, pipelines = pickle.load(file)
+            ilrs, pipeline_names, pipelines = pickle.load(file)
     else:
-        ilrs, pipelines = ([], [])
+        ilrs, pipeline_names, pipelines = ([], [], [])
     pp.pprint([(i, ilr.get_info()) for i, ilr in enumerate(ilrs)])
     
-    logging.info(F'Finished fitting IsotonicLogisticRegressions. ')
+    logger.info(F'Finished fitting IsotonicLogisticRegressions. ')
     
-    p = multiprocessing.Pool(args.ncores)
-    ret = list(p.map(patientwise_predict, [(ilrs, pipelines, infile, args.suffix, peplens, args.baseline, listof_features) for infile in args.test]))
+    map_args = [(ilrs, pipeline_names, pipelines, infile, args.suffix, peplens, args.baseline, listof_features) for infile in args.test]
+    if args.ncores == -2:
+        ret = list(map(patientwise_predict, map_args))
+    else:
+        ret = Parallel(n_jobs=args.ncores)(delayed(patientwise_predict)(arg) for arg in map_args)
+
     if tmp_output: tmp_output.close()
-    logging.info(F'Finished running {sys.argv[0]}')
+    logger.info(F'Finished running {sys.argv[0]} with ret={ret}')
     
 if __name__ == '__main__':
     main()
