921b78821a324ff21133f7f630ba5cd4686b4c69
ZhaoXiaofei <cndfeifei@hotmail.com>
2025-05-29 02:59:14 +0800

    modified:   benchmark_common/custom_models.py
    Added Logistic Reg model that generates groupwise constant output.
    This model does not work within a typical sklearn ML pipeline due to intermediate steps generating numpy array.

--- benchmark_common/custom_models.py
+++ benchmark_common/custom_models.py
@@ -29,20 +29,24 @@ class FixedZeroLogisticRegression(LogisticRegression):
         self.intercept_ = np.log(_logit(np.sum(y), len(y)))
         return self
 
-class FixedPatientLogisticRegression(LogisticRegression):
-    def __init__(self, **kwargs):
+class GroupEffectLogisticRegression(LogisticRegression):
+    def __init__(self, group_name='ln_NumTested', **kwargs):
         # Ensure no penalty (L2/L1 regularization would change coefficients)
         kwargs['penalty'] = None
         kwargs['random_state'] = 0
         super().__init__(max_iter=1, solver='sag', **kwargs)
         self.lr = LogisticRegression(solver='liblinear', random_state=0)
+        self.group_name = group_name
     def fit(self, X, y):
+        idx = -1
+        for i, c in enumerate(X.columns):
+            if c == self.group_name: idx = i
         X = np.array(X)
         super().fit(X, y)
-        self.lr.fit(X[:,[-1]], y)
+        self.lr.fit(X[:,[idx]], y)
         n_features = X.shape[1]
         self.coef_ = np.zeros((1, n_features))  # For binary classification
-        self.coef_[-1] = self.lr.coef_[-1]
+        self.coef_[idx] = self.lr.coef_[0]
         self.intercept_ = self.lr.intercept_
         return self
 
