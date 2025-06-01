544a35fb5923debc7958bad5b05dbfff55bc8d8f
ZhaoXiaofei <cndfeifei@hotmail.com>
2025-06-01 14:34:30 +0800

    modified: benchmark_common/custom_models.py (This commit does not change the benchmarking result)
    Aded HardThresholdClassifier

--- benchmark_common/custom_models.py
+++ benchmark_common/custom_models.py
@@ -1,3 +1,5 @@
+import warnings
+from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 
@@ -13,7 +15,7 @@ class FixedOneLogisticRegression(LogisticRegression):
         super().fit(X, y)
         n_features = X.shape[1]
         self.coef_ = np.ones((1, n_features))  # For binary classification
-        self.intercept_ = np.zeros(1)           # No bias
+        self.intercept_ = np.zeros(1)          # No bias
         return self
 
 class FixedZeroLogisticRegression(LogisticRegression):
@@ -49,4 +51,43 @@ class GroupEffectLogisticRegression(LogisticRegression):
         self.coef_[idx] = self.lr.coef_[0]
         self.intercept_ = self.lr.intercept_
         return self
+
+class HardThresholdClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, columns=[], op=-1, thres=0, **kwargs):
+        assert op in [-2,-1,0,1,2], f'The operator {op} is invalid!'
+        self.columns = columns
+        self.op = op
+        self.thres = thres
+    def fit(self, X, y, *args, **kwargs):
+        if hasattr(X, 'columns'):
+            self.fit_columns_ = X.columns
+            self.feature_names_in_ = X.columns.tolist()
+        else:
+            self.fit_columns_ = []
+        self.classes_ = np.unique(y)
+        self.n_features_in_ = X.shape[1]
+        return self
+    def predict(self, X):
+        def bin_operator(a, b):
+            if self.op ==-2: return a <  b
+            if self.op ==-1: return a <= b
+            if self.op == 0: return a == b
+            if self.op == 1: return a >= b
+            if self.op == 2: return a >  b
+        if hasattr(X, 'columns'):
+            predict_columns = X.columns
+            if len(self.fit_columns_) > 0:
+                assert len(self.fit_columns_) == len(predict_columns), F'{len(self.fit_columns_)} == {len(predict_columns)} failed!'
+                assert (self.fit_columns_ == predict_columns).all(), F'{self.fit_columns} == {predict_columns} failed!'
+        else:
+            predict_columns = self.fit_columns_
+        X2 = np.array(X)
+        for col_idx, col_name in enumerate(predict_columns):
+            if col_name in self.columns:
+                return np.where(bin_operator(X2[:,col_idx], self.thres), 1, 0)
+        warnings.warn(f'The array-like {X} has no column named {self.columns}!')
+        return np.zeros(len(X))
+    def predict_proba(self, X):
+        ret = self.predict(X)
+        return np.array([(1-p, p) for p in ret])
 
